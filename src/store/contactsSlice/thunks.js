f23bc8be4dca5e9ad7a582c371638b02f65c4bbc
IYuliia <catnew2012@gmail.com>
2024-03-17 00:04:24 +0100

    updated thunks.js

--- src/store/contactsSlice/thunks.js
+++ src/store/contactsSlice/thunks.js
@@ -1,3 +1,24 @@
+// import { createAsyncThunk } from '@reduxjs/toolkit';
+// import {
+//   addContactApi,
+//   deleteContactApi,
+//   fetchContactsApi,
+// } from 'api/contacts';
+
+// export const fetchContactsThunk = createAsyncThunk(
+//   'contacts/fetchAll',
+//   () => fetchContactsApi
+// );
+
+// export const addContactThunk = createAsyncThunk(
+//   'contacts/addContact',
+//   () => addContactApi
+// );
+
+// export const deleteContactThunk = createAsyncThunk(
+//   'contacts/deleteContact',
+//   () => deleteContactApi
+// );
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import {
   addContactApi,
@@ -7,15 +28,36 @@ import {
 
 export const fetchContactsThunk = createAsyncThunk(
   'contacts/fetchAll',
-  () => fetchContactsApi
+  async (_, thunkAPI) => {
+    try {
+      const data = await fetchContactsApi();
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
 );
 
 export const addContactThunk = createAsyncThunk(
   'contacts/addContact',
-  () => addContactApi
+  async (contact, thunkAPI) => {
+    try {
+      const data = await addContactApi(contact);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
 );
 
 export const deleteContactThunk = createAsyncThunk(
   'contacts/deleteContact',
-  () => deleteContactApi
+  async (id, thunkAPI) => {
+    try {
+      const data = await deleteContactApi(id);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
 );
